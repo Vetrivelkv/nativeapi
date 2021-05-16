7d98620251784c7f9e95218954674a49c66cbd4e
Vetrivelkv <vetrvelkvk@gmail.com>
2021-05-16 15:05:21 +0530

    work

--- dbconfig.js
+++ dbconfig.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 
 var con = mysql.createConnection({
-    host: "remotemysql.com",
-    user: "8Lz8m6nJyh",
-    password: "97cawUiVl7",
-    database: '8Lz8m6nJyh'
+    host: "sql6.freemysqlhosting.net",
+    user: "sql6412753",
+    password: "Pk4zjlLLj9",
+    database: 'sql6412753'
   
   });
   
--- sms.js
+++ sms.js
@@ -1,3 +1,17 @@
+// const fast2sms = require('fast-two-sms')
+// const con = require('./dbconfig');
+
+
+// module.exports.sendSMS = function  (otp, number){
+// console.log("OTP :"+otp)
+// console.log("Number :"+number)
+//  var options = {authorization : 'eurYepf21FrJ0VKF9q8nhMXXt6SL9xDJBsNVAoZjLxhpG2uNEiUOahlL3yTj' , message : ''+otp ,  numbers : [number]} 
+//  const msg=fast2sms.sendMessage(options)
+//  //console.log(msg)
+
+// }
+
+
 const fast2sms = require('fast-two-sms')
 const con = require('./dbconfig');
 
@@ -7,7 +21,6 @@ console.log("OTP :"+otp)
 console.log("Number :"+number)
  var options = {authorization : 'eurYepf21FrJ0VKF9q8nhMXXt6SL9xDJBsNVAoZjLxhpG2uNEiUOahlL3yTj' , message : ''+otp ,  numbers : [number]} 
  const msg=fast2sms.sendMessage(options)
- //console.log(msg)
+ console.log(msg)
 
 }
-
